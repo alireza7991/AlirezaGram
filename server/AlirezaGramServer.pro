dca4918ec4b27ac4aa0b0639be82f592183c2f67
alireza7991 <alirezafn@gmx.us>
2016-06-23 03:01:56 +0430

    add udp server and datagram loop

--- server/AlirezaGramServer.pro
+++ server/AlirezaGramServer.pro
@@ -1,4 +1,4 @@
-QT += core
+QT += core network
 QT -= gui
 
 CONFIG += c++11
--- server/main.cpp
+++ server/main.cpp
@@ -1,8 +1,27 @@
 #include <QCoreApplication>
+#include <QDebug>
+#include <QUdpSocket>
+#include <iostream>
+
+#define SERVER_PORT 7991
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    cout << "Starting server ..." << endl;
+    QUdpSocket *serverSocket = new QUdpSocket(0);
+    serverSocket->bind(SERVER_PORT,QUdpSocket::ShareAddress);
+    while (serverSocket->waitForReadyRead(-1)) {
+           while(serverSocket->hasPendingDatagrams()) {
+               QByteArray datagram;
+               datagram.resize(serverSocket->pendingDatagramSize());
+               QHostAddress sender;
+               quint16 senderPort;
+               serverSocket->readDatagram(datagram.data(), datagram.size(),&sender, &senderPort);
+               cout << "datagram received from " << sender.toString().toStdString() << " "<<datagram.data()<<endl;
+           }
+       }
     return a.exec();
 }
