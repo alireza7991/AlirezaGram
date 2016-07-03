3006464f01d48ba71b16c75b5d0d3e99f0aabf72
alireza7991 <alirezafn@gmx.us>
2016-07-03 20:30:09 +0430

    implement protocol on client-side

--- client/main.cpp
+++ client/main.cpp
@@ -9,8 +9,222 @@
 #include <QImage>
 #include <QPixmap>
 #include <QString>
+#include <string>
+#include <iostream>
 #include <AlirezaSocket.h>
 #include <opencvQtWrapper.h>
+
+#define SERVER_IP (char *)"127.0.0.1"
+#define SERVER_PORT 7991
+
+struct msg {
+    string sender;
+    string content;
+};
+
+class AlirezaLogin : public AlirezaSocket {
+public:
+    string u,p;
+    bool result = false;
+    string sid;
+    void onConnect(SOCKET c) {
+        string out = "l";
+        out +=(char)(u.length()+48);
+        out += u;
+        out +=(char)(p.length()+48);
+        out += p;
+        cout << "command: " << out << endl;
+        sendString(c,out);
+        string res=recieveString(c);
+        if(res=="lf") {
+            result = false;
+            closesocket(c);
+        }
+        sid = res.substr(2,string::npos);
+        result = true;
+        closesocket(c);
+    }
+};
+
+class AlirezaGetContacts : public AlirezaSocket {
+public:
+    string sid;
+    vector<string> contacts;
+    bool result = false;
+    void onConnect(SOCKET c) {
+        string out = "c";
+        out += sid;
+        cout << "command: " << out << endl;
+        sendString(c,out);
+        string res=recieveString(c);
+        if(res=="esid") {
+            result = false;
+            closesocket(c);
+        }
+        res += ':';
+        int lastPosition = 0;
+        bool continueLoop = true;
+        while (continueLoop) {
+             unsigned int pos = res.find(':', lastPosition);
+             if (pos == res.length() - 1) continueLoop = false;
+             contacts.push_back(res.substr(lastPosition, pos - lastPosition));
+             lastPosition = pos + 1;
+         }
+        result = true;
+        closesocket(c);
+    }
+};
+
+
+class AlirezaSignup : public AlirezaSocket {
+public:
+    string u,p;
+    bool result = false;
+    string sid;
+    void onConnect(SOCKET c) {
+        string out = "s";
+        out +=(char)(u.length()+48);
+        out += u;
+        out +=(char)(p.length()+48);
+        out += p;
+        cout << "command: " << out << endl;
+        sendString(c,out);
+        string res=recieveString(c);
+        if(res=="sf") {
+            result = false;
+            closesocket(c);
+        }
+        sid = res.substr(2,string::npos);
+        result = true;
+        closesocket(c);
+    }
+};
+
+class AlirezaWriteMessage : public AlirezaSocket {
+public:
+    string sid,rec,con;
+    bool result = false;
+    void onConnect(SOCKET c) {
+        string out = "w";
+        out += sid;
+        out += rec;
+        out += ':';
+        out += con;
+        cout << "command: " << out << endl;
+        sendString(c,out);
+        string res=recieveString(c);
+        if(res!="mo") {
+            cout << "send message failed with " << res << endl;
+            result = false;
+            closesocket(c);
+        }
+        result = true;
+        closesocket(c);
+    }
+};
+
+class AlirezaReadMessage : public AlirezaSocket {
+public:
+    string sid;
+    bool result = false;
+    vector<msg> msgs;
+    void onConnect(SOCKET c) {
+        string out = "r";
+        out +=sid;
+        cout << "command: " << out << endl;
+        sendString(c,out);
+        string res=recieveString(c);
+        if(res.substr(0,2)!="mo") {
+            cout << "read messages failed with " << res << endl;
+            result = false;
+            closesocket(c);
+        }
+        int n = stoi(res.substr(2,string::npos));
+        cout << "unread messages " << n << endl;
+        for(int i=0; i<n;i++) {
+            string smsg = recieveString(c);
+            unsigned mdiv = smsg.find(':',0);
+            string sender = smsg.substr(0,mdiv);
+            string content = smsg.substr(mdiv+1,string::npos);
+            cout << "recieved msg : " << sender<< " content: " << content << endl;
+            msg ms;
+            ms.sender = sender;
+            ms.content = content;
+            msgs.push_back(ms);
+        }
+        result = true;
+        closesocket(c);
+    }
+};
+
+bool Alogin(string user,string pass,string &sid) {
+    try {
+        AlirezaLogin a;
+        a.u=user;
+        a.p=pass;
+        a.Connect(SERVER_IP,SERVER_PORT);
+        sid=a.sid;
+        return a.result;
+    } catch(runtime_error e) {
+        cout << e.what() << endl;
+        return false;
+    }
+}
+
+bool Asignup(string user,string pass,string &sid) {
+    try {
+        AlirezaSignup a;
+        a.u=user;
+        a.p=pass;
+        a.Connect(SERVER_IP,SERVER_PORT);
+        sid=a.sid;
+        return a.result;
+    } catch(runtime_error e) {
+        cout << e.what() << endl;
+        return false;
+    }
+
+}
+
+bool Agetcontacts(string sid,vector<string> &contacts) {
+    try {
+        AlirezaGetContacts a;
+        a.sid=sid;
+        a.Connect(SERVER_IP,SERVER_PORT);
+        contacts=a.contacts;
+        return a.result;
+    } catch(runtime_error e) {
+        cout << e.what() << endl;
+        return false;
+    }
+}
+
+bool Agetmsgs(string sid,vector<msg> &msgs) {
+    try {
+        AlirezaReadMessage a;
+        a.sid=sid;
+        a.Connect(SERVER_IP,SERVER_PORT);
+        msgs=a.msgs;
+        return a.result;
+    } catch(runtime_error e) {
+        cout << e.what() << endl;
+        return false;
+    }
+}
+
+bool Asendmsg(string sid,string reciever,string content) {
+    try {
+        AlirezaWriteMessage a;
+        a.sid=sid;
+        a.con=content;
+        a.rec=reciever;
+        a.Connect(SERVER_IP,SERVER_PORT);
+        return a.result;
+    } catch(runtime_error e) {
+        cout << e.what() << endl;
+        return false;
+    }
+}
 
 int main(int argc, char *argv[])
 {
